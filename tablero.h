8472b6d362d6c533174129f688dfa8315dd2a22a
Florencia-97 <florrr1997@gmail.com>
2020-05-19 11:50:59 -0300

    Cambio tabla por lista, para facilitar mips

--- tablero.h
+++ tablero.h
@@ -6,22 +6,19 @@
 int ZOOM_ARCHIVO = 30; // Mantener un número múltiplo de 10
 
 typedef struct tablero{
-   int** tabla;
+   int* tabla;
    int l;
    int h;
 }tablero_t;
 
 void tablero_eliminar(tablero_t* self);
 
+// Para facilitar la tarea de mips, la grilla es realmente una lista
+// que es luego tratada como un grilla
 tablero_t* tablero_crear(int length, int height){
     tablero_t* tablero = (tablero_t*) malloc(sizeof(tablero_t));
-    tablero->tabla = (int**) malloc(height * sizeof(int *));
+    tablero->tabla = (int*) calloc(height * length , sizeof(int));
     if (!tablero->tabla) return NULL;
-    for (int i=0 ; i<height ; i++) {
-        tablero->tabla[i] = (int*) malloc(length*sizeof(int));
-        for (int j=0; j<length; j++)
-            tablero->tabla[i][j] = 0;
-    }
     tablero->l = length;
     tablero->h = height;
     return tablero;
@@ -34,8 +31,8 @@ int tablero_cargar_tablero(tablero_t* self, char* path){
     char buffer[4];
     while (fgets(buffer, 100, fp) != NULL){
         int fila = buffer[0] - '0';
-        int columna = buffer[2] - '0';
-        self->tabla[fila][columna] = 1;
+        int col = buffer[2] - '0';
+        self->tabla[fila*self->l + col] = 1;
     }
     fclose(fp);
     return 0;
@@ -45,14 +42,14 @@ int tablero_cargar_tablero(tablero_t* self, char* path){
 // de no ser posible modificar su valor retorna 1,
 // 0 en caso de modificarlo correctamente.
 void tablero_modificar_estado_campo(tablero_t* self, int fila, int col){
-    int actual = self->tabla[fila][col];
-    actual == 0 ? (self->tabla[fila][col] = 1) : (self->tabla[fila][col] = 0);
+    int actual = self->tabla[fila*self->l + col];
+    actual == 0 ? (self->tabla[fila*self->l + col] = 1) : (self->tabla[fila*self->l + col] = 0);
 }
 
 void imprimir_terminal(tablero_t* self){
     for (int i=0 ; i< self->h ; i++){
         for (int j=0 ; j< self->l ; j++){
-            printf("%i ", self->tabla[i][j]);
+            printf("%i ", self->tabla[i*self->l + j]);
         }
         printf("\n");
     }
@@ -73,7 +70,7 @@ void imprimir_archivo(tablero_t* self, int i, char* prefijo){
         for (int j=0 ; j< (self->l * ZOOM_ARCHIVO) ; j++){
             int x  = i / ZOOM_ARCHIVO;
             int y = j / ZOOM_ARCHIVO;
-            int vivo = self->tabla[x][y];
+            int vivo = self->tabla[x*self->l + y];
             if (vivo)
                 (void) fwrite(color_vivo, 1, 3, fp);
             else
@@ -109,7 +106,7 @@ unsigned int vecinos_mips(unsigned char *a, unsigned int i, unsigned int j,
 }
 
 
-unsigned int vecinos_c(int** a, unsigned int i, unsigned int j,
+unsigned int vecinos_c(int* a, unsigned int i, unsigned int j,
                     unsigned int M, unsigned int N){
     int contador = 0;
     for (int c1 = -1; c1 <= 1; ++c1){
@@ -117,7 +114,7 @@ unsigned int vecinos_c(int** a, unsigned int i, unsigned int j,
         for (int c2 = -1; c2 <= 1; ++c2){
             if (c1 == 0 && c2 == 0) continue;
             int c = mod( j + c2, M);
-            if (a[f][c] == 1) contador++;
+            if (a[f*M + c] == 1) contador++;
         }
     }
     return contador;
@@ -130,13 +127,13 @@ tablero_t* tablero_modificar(tablero_t* self){
         for (int j=0 ; j< self->l ; j++){
             unsigned int vecinos = vecinos_c(self->tabla, i, j, self->l, self->h);
             // unsigned int vecinos = vecinos_mips((unsigned char*)&self->tabla[0][0], i, j, self->l, self->h);
-            int viva = self->tabla[i][j];
+            int viva = self->tabla[i*self->l + j];
             if ((vecinos < 2) || (vecinos > 3))
-                tablero_nuevo->tabla[i][j] = 0;
+                tablero_nuevo->tabla[i*self->l + j] = 0;
             if (viva && (vecinos == 2 || vecinos == 3))
-                tablero_nuevo->tabla[i][j] = 1;
+                tablero_nuevo->tabla[i*self->l + j] = 1;
             if (!viva && vecinos == 3){
-                tablero_nuevo->tabla[i][j] = 1;
+                tablero_nuevo->tabla[i*self->l + j] = 1;
             }
         }
     }
@@ -147,7 +144,6 @@ tablero_t* tablero_modificar(tablero_t* self){
 
 // Elimina el talbero creado
 void tablero_eliminar(tablero_t* self){
-    for (int i=0 ; i< self->h ; i++) free(self->tabla[i]);
     free(self->tabla);
     free(self);
 }
