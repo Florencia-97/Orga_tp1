945f025e2ec87435ad2375dca4690576be7e2e34
Santiago Husain <santiago.husain@gmail.com>
2020-05-22 20:33:22 -0300

    Undefined reference fix.

--- src/vecinos.h
+++ src/vecinos.h
@@ -1,6 +1,8 @@
 #ifndef VECINOS__H
 #define VECINOS__H
 
+#include "mod.h"
+
 extern unsigned int vecinos(unsigned char *a,
                             unsigned int i, unsigned int j,
                             unsigned int M, unsigned int N);
